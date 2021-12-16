592d4827358747642b5ca4660bc918626b130999
henrygonzalez1mt <henry.gonzalez1.mt@correo.usa.edu.co>
2021-12-15 22:45:00 -0500

    backend reto 4 Ok_a

---
+++ src/main/java/co/edu/usa/ciclo3/repository/crud/OrderCrudRepository.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.usa.ciclo3.repository.crud;
+
+/**
+ *
+ * @author hgc68
+ */
+
+import co.edu.usa.ciclo3.modelo.Order;
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface OrderCrudRepository extends MongoRepository<Order, Integer>{
+    List<Order> findBySalesManZone(String zone);
+    
+}
